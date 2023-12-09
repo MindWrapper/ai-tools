0606b6bc5d8e9a21f63844cdd3662055a3c954d4
Yan Drugalya <yan@unity3d.com>
2023-12-09 22:32:26 +0100

    Add copy or execute promt

--- aicmd.sh
+++ aicmd.sh
@@ -8,7 +8,7 @@ PROMPT="print command that does '${AI_COMMAND}'.
 If user asks to search or find something, then make sure to include hidden files.
 If user doesn't specify the scope of search command, then use current directory.
 Output should contain command only, I can paste into terminal. No comments or anything else.
-Output should be a single line."
+Output should be a single line and signle command."
 
 RESPONSE=$(curl -s -X POST "https://api.openai.com/v1/chat/completions" \
     -H "Authorization: Bearer $OPEN_AI_KEY" \
@@ -25,8 +25,24 @@ EOF
 COMMAND=$(echo $RESPONSE | jq -r '.choices[0].message.content')
 
 # Check and display the command
+# Check and offer options
 if [ -n "$COMMAND" ]; then
     echo "$COMMAND"
+    echo "(c)opy or (e)xecute?"
+    read -r choice
+
+    case $choice in
+        c|C)
+            echo "$COMMAND" | pbcopy  # For macOS. Use 'xclip -selection clipboard' or 'xsel --clipboard --input' on Linux
+            echo "Command copied to clipboard."
+            ;;
+        e|E)
+            eval "$COMMAND"
+            ;;
+        *)
+            echo "Invalid choice. Exiting."
+            ;;
+    esac
 else
     echo "Cannot generate command. Try another prompt."
 fi
